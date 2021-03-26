26c3eaba5657a9638509c857b9d25e3bc344a3e4
Spenser Haddad <spenser.haddad@gmail.com>
2021-03-25 23:02:28 -0400

    Add dev dependencies and update version

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-import sys
-
 from setuptools import setup
 from setuptools_rust import RustExtension
 
 setup(
     name="uwufierpy",
-    version="0.1.0",
+    version="1.0.0",
     packages=["uwufier"],
     rust_extensions=[RustExtension("uwufier.uwupy")],
+    extras_require={"dev": ["pytest", "flake8", "black"]},
     include_package_data=True,
     zip_safe=False,
 )

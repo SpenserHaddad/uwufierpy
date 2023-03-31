5a367164d4a773edbc1b49d18993f074f20cbd97
Spenser Haddad <spenser.haddad@gmail.com>
2023-03-30 20:02:35 -0400

    Migrate project definition to pyproject.toml

--- setup.py
+++ setup.py
@@ -3,11 +3,5 @@ from setuptools import setup
 from setuptools_rust import RustExtension
 
 setup(
-    name="uwufierpy",
-    version="1.0.0",
-    packages=["uwufier"],
     rust_extensions=[RustExtension("uwufier.uwupy")],
-    extras_require={"dev": ["pytest", "flake8", "black"]},
-    include_package_data=True,
-    zip_safe=False,
 )
